f81d1f09f465556b7a15c6cdb968a590a170c3e4
Mathieu <60007958+gojoise@users.noreply.github.com>
2022-02-28 17:11:11 +0100

    [EDC-2788] home improvements (#54)
    - add a clickable link in the logo to return to home
    - add custom css integration on home page
    
    Story [EDC-2789]
    Epic [EDC-2683]

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -6,19 +6,25 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 import { of } from 'rxjs';
+import { ConfigService } from 'app/config.service';
 
 describe('Home Component', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
   let helpService: HelpService;
   let translateService: TranslateService;
+  let configService: ConfigService;
 
   const title = 'foo';
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [HomeComponent],
-      providers: [mockService(HelpService, ['getTitle']), mockService(TranslateService, ['get'])],
+      providers: [
+        mockService(HelpService, ['getTitle']),
+        mockService(TranslateService, ['get']),
+        mockService(ConfigService, ['getConfiguration']),
+      ],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   }));
@@ -32,8 +38,10 @@ describe('Home Component', () => {
   beforeEach(() => {
     helpService = TestBed.inject(HelpService);
     translateService = TestBed.inject(TranslateService);
+    configService = TestBed.inject(ConfigService);
     spyOn(helpService, 'getTitle').and.returnValue(of(title));
     spyOn(translateService, 'get').and.returnValue(of(title));
+    spyOn(configService, 'getConfiguration').and.returnValue({ documentationStylePath: '/assets/style/custom.css' });
   });
 
   beforeEach(() => {
