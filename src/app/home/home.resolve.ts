5f10b9f98e777a0ccd63e27626b4cec18811b77e
Daniel <daniel.mayer@tech-advantage.com>
2022-03-16 10:10:39 +0100

    [EDC-2933] Home: target a specific export
    - add routes to home with plugin and lang parameters
    - reload export if target export is not the current one
    
    Story [EDC-2789]

--- src/app/home/home.resolve.ts
+++ src/app/home/home.resolve.ts
@@ -1,23 +1,56 @@
 import { EMPTY, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
-import { Resolve } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { HelpActions } from '../ngrx/actions/help.actions';
 import { HelpService } from '../help/help.service';
 import { ExportInfo } from 'edc-client-js';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.state';
+import { LANG_PARAM, PLUGIN_PARAM } from '../context/context.constants';
+import { selectExportId } from '../ngrx/selectors/help-selectors';
 
 @Injectable()
 export class HomeResolve implements Resolve<unknown> {
-  constructor(private readonly helpService: HelpService, private readonly helpActions: HelpActions) {}
+  constructor(
+    private readonly helpService: HelpService,
+    private readonly helpActions: HelpActions,
+    private readonly store: Store<AppState>
+  ) {}
 
-  resolve(): Observable<void> {
-    // Initialize documentation state's export id and language
-    this.helpService.getContent().subscribe((exportInfo: ExportInfo) => {
-      if (exportInfo != null) {
-        this.helpActions.setExportId(exportInfo.pluginId);
-        this.helpActions.setDocumentationLanguage(exportInfo.currentLanguage);
-      }
-    });
+  resolve(route: ActivatedRouteSnapshot): Observable<unknown> {
+    // Check if plugin id (export id) was provided within parameters
+    const pluginId: string = route.params[PLUGIN_PARAM];
+    if (pluginId) {
+      // Check if it matches any currently loaded export
+      this.store.select(selectExportId).pipe(
+        tap((exportId: string) => {
+          if (exportId !== pluginId) {
+            // Not current export, reload export with new export id
+            const lang: string = route.params[LANG_PARAM];
+            this.helpService.connect(pluginId, lang);
+          }
+          this.initContent();
+        })
+      );
+    } else {
+      this.initContent();
+    }
     return EMPTY;
+  }
+
+  private initContent(): void {
+    this.helpService
+      .getContent()
+      .pipe(
+        tap((exportInfo: ExportInfo) => {
+          if (exportInfo != null) {
+            this.helpActions.setExportId(exportInfo.pluginId);
+            this.helpActions.setDocumentationLanguage(exportInfo.currentLanguage);
+          }
+        })
+      )
+      .subscribe();
   }
 }
--- src/app/home/home.routes.ts
+++ src/app/home/home.routes.ts
@@ -1,10 +1,25 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { HomeResolve } from './home.resolve';
+import { LANG_PARAM, PLUGIN_PARAM } from '../context/context.constants';
 
 export const homeRoutes: Routes = [
   {
     path: 'home',
+    component: HomeComponent,
+    resolve: {
+      documentation: HomeResolve,
+    },
+  },
+  {
+    path: `home/:${PLUGIN_PARAM}`,
+    component: HomeComponent,
+    resolve: {
+      documentation: HomeResolve,
+    },
+  },
+  {
+    path: `home/:${PLUGIN_PARAM}/:${LANG_PARAM}`,
     component: HomeComponent,
     resolve: {
       documentation: HomeResolve,
