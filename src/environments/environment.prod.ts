1297e827d8f9fdd82ef88474e6b07315756b628e
bracq <florianbracq42@gmail.com>
2022-11-24 00:29:34 +0100

    Update jasmine package and build config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
-import { version } from '../../package.json';
+import packageInfo from '../../package.json';
 
 export const environment = {
   production: true,
   hmr: false,
   configFile: 'assets/config.json',
-  version: version,
+  version: packageInfo.version,
 };
